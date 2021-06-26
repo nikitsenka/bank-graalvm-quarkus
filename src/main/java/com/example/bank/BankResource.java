62e0cebe3cbfc82857ebe0c3cda1a80530816ffa
INiki1 <ivan.nikitsenka@nike.com>
2021-06-26 06:23:31 +0200

    Bank implementation

--- src/main/java/com/example/bank/BankResource.java
+++ src/main/java/com/example/bank/BankResource.java
@@ -20,7 +20,6 @@ import io.smallrye.mutiny.Uni;
 import io.vertx.mutiny.pgclient.PgPool;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
-import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -31,6 +30,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
+import java.util.function.Function;
 
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
@@ -44,7 +44,7 @@ public class BankResource {
     @Path("client/{id}/balance")
     public Uni<Response> getBalance(@PathParam Integer id) {
         return Bank.getBalance(pgClient, id)
-                .onItem().transform(balance -> balance != null ? Response.ok(balance) : Response.status(Status.NOT_FOUND))
+                .onItem().transform(toHttpOk())
                 .onItem().transform(ResponseBuilder::build);
     }
 
@@ -53,7 +53,20 @@ public class BankResource {
     public Uni<Response> createClient(@PathParam Integer balance) {
         return Bank.createClient(pgClient, new Client(0, "", "", ""))
                 .onItem().call(client -> Bank.createTransaction(pgClient, new Transaction(0, 0, client.getId(), balance)))
-                .onItem().transform(client -> client != null ? Response.ok(client) : Response.status(Status.NOT_FOUND))
+                .onItem().transform(toHttpOk())
                 .onItem().transform(ResponseBuilder::build);
+    }
+
+
+    @POST
+    @Path("transaction")
+    public Uni<Response> createClient(Transaction transaction) {
+        return Bank.createTransaction(pgClient, transaction)
+                .onItem().transform(toHttpOk())
+                .onItem().transform(ResponseBuilder::build);
+    }
+
+    private <T> Function<T, ResponseBuilder> toHttpOk() {
+        return r -> r != null ? Response.ok(r) : Response.status(Status.NOT_FOUND);
     }
 }
